5164f2da93133adc4a92a2fe4f9d07f545327c7a
Arthur Lucas-le guilly <arthurlucas2002@hotmail.com>
2022-04-08 11:07:09 +0200

    Partage pas fonctionnel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,24 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule } from '@angular/common/http';
 import { IonicStorageModule } from '@ionic/storage-angular';
 
-
+import { SocialSharing } from '@awesome-cordova-plugins/social-sharing/ngx';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule,IonicStorageModule.forRoot()],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+    IonicStorageModule.forRoot(),
+  ],
   providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+    SocialSharing,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/detail-hackathon/detail-hackathon.page.ts
+++ src/app/detail-hackathon/detail-hackathon.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NavigationExtras } from '@angular/router';
+import { SocialSharing } from '@awesome-cordova-plugins/social-sharing/ngx';
 
 @Component({
   selector: 'app-detail-hackathon',
@@ -10,8 +11,23 @@ import { NavigationExtras } from '@angular/router';
 })
 export class DetailHackathonPage implements OnInit {
   item = '';
+  image = '';
+  theme = '';
+  dateDebut = '';
+  dateFin = '';
+  heureDebut = '';
+  dateLimite = '';
+  lieu = '';
+  rue = '';
+  ville = '';
+  cp = '';
+  nbPlace = '';
 
-  constructor(private router: Router, private activeRoute: ActivatedRoute) {
+  constructor(
+    private router: Router,
+    private activeRoute: ActivatedRoute,
+    private socialSharing: SocialSharing
+  ) {
     this.activeRoute.queryParams.subscribe((params) => {
       if (this.router.getCurrentNavigation().extras.state) {
         this.item = this.router.getCurrentNavigation().extras.state.param1;
@@ -28,5 +44,26 @@ export class DetailHackathonPage implements OnInit {
       },
     };
     this.router.navigate(['/atelier'], navigationExtras);
+  }
+
+  MonClickPartager() {
+    // Share via email
+    this.socialSharing
+      .shareViaEmail(
+        this.image,
+        this.theme,
+        this.dateDebut,
+        this.dateFin,
+        this.heureDebut,
+        this.dateLimite,
+        this.lieu,
+        this.rue,
+        this.ville
+        this.cp,
+        this.nbPlace
+      )
+      .then(() => {
+        // Success!
+      });
   }
 }
